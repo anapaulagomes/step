abf8d40bf4b63e151d262df720a658acbb196039
Ana Paula Gomes <apgomes88@gmail.com>
2023-07-07 22:42:25 +0200

    Create python code based in templates

--- step/cli.py
+++ step/cli.py
@@ -1,3 +1,4 @@
+# import pprint
 from pathlib import Path
 
 import typer
@@ -8,14 +9,41 @@ app = typer.Typer()
 
 
 def generate_cli_from(cli_name: str, markdown_filepath: Path):
-    cli_path = Path(cli_name)
     name, description, checklist = from_markdown_to_steps(markdown_filepath)
-    # TODO create python file
-    return cli_path
+    if not cli_name:
+        cli_name = name
+
+    cli_template = Path("step/templates/cli.py.template").read_text()
+    cli_template = cli_template.replace("STEP_NAME_HERE", cli_name)
+    cli_template = cli_template.replace("STEP_DESCRIPTION_HERE", description)
+
+    cli_filepath = (
+        markdown_filepath.parent / f"cli_{cli_name.lower().replace(' ', '_')}.py"
+    )
+
+    commands = []
+    for step in checklist:
+        title = step.title.lower().replace(
+            " ", "_"
+        )  # TODO make sure it is valid python
+        command_template = Path("step/templates/command.py.template").read_text()
+        command_template = command_template.replace("STEP_TITLE_HERE", title)
+        command_template = command_template.replace(
+            "STEP_DESCRIPTION_HERE", step.description
+        )
+        commands.append(command_template)
+        # TODO add sub_steps (STEP_SUB_STEPS)
+        # FIXME deal with break lines from markdown
+
+    # pretty_commands = pprint.pformat(commands, indent=4)
+    cli_template = cli_template.replace("STEP_CHECKLIST_HERE", "\n\n".join(commands))
+    cli_filepath.write_text(cli_template)
+
+    return cli_filepath
 
 
 @app.command()
-def main(cli_name: str, markdown_checklist: Path):
+def main(markdown_checklist: Path, cli_name: str = ""):
     filepath = generate_cli_from(cli_name, markdown_checklist)  # TODO generate cli
     typer.echo(f"Your CLI is available in: {filepath}.")
 
--- step/step.py
+++ step/step.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass, field
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Optional
+
+
+def callback(*args, **kwargs):
+    pass
 
 
 @dataclass
@@ -7,4 +11,4 @@ class Step:
     title: str
     description: str = ""
     sub_steps: List[Any] = field(default_factory=list)
-    function: Callable = lambda x: x  # noqa
+    function: Optional[Callable] = field(default_factory=callback, repr=False)
